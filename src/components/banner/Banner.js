0bfeed8fa1f6f05ba5ba34f599d394a6a7d11f59
mahmood2122 <mahmoodsino40@gmail.com>
2023-07-04 13:49:36 +0300

    add particle

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -1,16 +1,124 @@
-import React from 'react'
-import LeftBanner from './LeftBanner';
-import RightBanner from './RightBanner';
+import React from "react";
+import Particles from "react-tsparticles";
+import LeftBanner from "./LeftBanner";
+import RightBanner from "./RightBanner";
 const Banner = () => {
   return (
     <section
       id="home"
-      className="w-full pt-10 pb-10 flex flex-col gap-10 xl:gap-0 lgl:flex-row items-center border-b-[1px] font-titleFont border-b-black"
+      className="w-full pt-10 z-10 pb-10 overflow-hidden flex flex-col gap-10 xl:gap-0 lgl:flex-row  lgl:items-start border-b-[1px] font-titleFont border-b-black "
     >
       <LeftBanner />
-     <RightBanner />
+      <div className="relative flex justify-center items-center lgl:w-[50%] h-full">
+        <Particles
+          id="particles-js"
+          options={{
+            fullScreen: {
+              enable: true,
+              zIndex: 0,
+            },
+            style: { position: "absolute" },
+            particles: {
+              number: {
+                value: 150,
+                density: {
+                  enable: true,
+                  value_area: 800,
+                },
+              },
+              color: {
+                value: "#ffffff",
+              },
+              shape: {
+                type: "circle",
+                stroke: {
+                  width: 0,
+                  color: "#000000",
+                },
+                polygon: {
+                  nb_sides: 5,
+                },
+              },
+              size: {
+                value: 3,
+                random: true,
+                anim: {
+                  enable: false,
+                  speed: 40,
+                  size_min: 0.1,
+                  sync: false,
+                },
+              },
+              line_linked: {
+                enable: true,
+                distance: 150,
+                color: "#ffffff",
+                opacity: 0.4,
+                width: 1,
+              },
+              move: {
+                enable: true,
+                speed: 1,
+                direction: "none",
+                random: false,
+                straight: false,
+                out_mode: "out",
+                bounce: false,
+                attract: {
+                  enable: false,
+                  rotateX: 600,
+                  rotateY: 1200,
+                },
+              },
+            },
+            interactivity: {
+              detect_on: "canvas",
+              events: {
+                onhover: {
+                  enable: true,
+                  mode: "repulse",
+                },
+                onclick: {
+                  enable: true,
+                  mode: "push",
+                },
+                resize: true,
+              },
+              modes: {
+                grab: {
+                  distance: 400,
+                  line_linked: {
+                    opacity: 1,
+                  },
+                },
+                bubble: {
+                  distance: 400,
+                  size: 40,
+                  duration: 2,
+                  opacity: 8,
+                  speed: 3,
+                },
+                repulse: {
+                  distance: 100,
+                  duration: 0.4,
+                },
+                push: {
+                  particles_nb: 4,
+                },
+                remove: {
+                  particles_nb: 2,
+                },
+              },
+            },
+            retina_detect: true,
+          }}
+        />
+        <div className="z-10">
+          <RightBanner />
+        </div>
+      </div>
     </section>
   );
-}
+};
 
-export default Banner
+export default Banner;
--- src/components/banner/RightBanner.js
+++ src/components/banner/RightBanner.js
@@ -2,14 +2,13 @@ import React from 'react'
 
 const RightBanner = () => {
   return (
-    <div className="w-full lgl:w-1/2 flex justify-center items-center relative">
+    <div className="w-full flex justify-center items-center relative">
       <img
         className=""
         src="/bannerImg.png"
         alt="bannerImg"
       />
-      {/* <img src='/profile-pic.png' /> */}
-      {/* <div className="absolute bottom-0 w-[350px] h-[300px] lgl:w-[500px] lgl:h-[500px] bg-gradient-to-r from-[#303036] to-[#2e2e38] shadow-shadowOne flex justify-center items-center"></div> */}
+     
     </div>
   );
 }
