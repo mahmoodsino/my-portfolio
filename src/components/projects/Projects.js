c999f6bc9698fb05af34f845f0e53d20e0a78da6
mahmood2122 <mahmoodsino40@gmail.com>
2023-07-10 16:16:08 +0300

    fix

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -71,7 +71,7 @@ const Projects = () => {
         />
         <ProjectsCard
           title="search feature For Ferm"
-          des="using Next App Route ,project for FAO allows users to search for and view practice materials. The platform offers options to print or download the content as a PDF. It provides a convenient way to access and utilize practice resources effectively. The FAO project enhances accessibility and usability of practice materials for users."
+          des="Using Next App Route ,project for FAO allows users to search for and view practice materials. The platform offers options to print or download the content as a PDF. It provides a convenient way to access and utilize practice resources effectively. The FAO project enhances accessibility and usability of practice materials for users."
           isNx
           isNext
           isReact
