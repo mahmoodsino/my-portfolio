eee270b7efceeb333305eb6bc0d499c2eb126fdd
mahmood2122 <mahmoodsino40@gmail.com>
2024-03-02 14:37:01 +0400

    fix

--- src/components/resume/Education.js
+++ src/components/resume/Education.js
@@ -9,13 +9,7 @@ const Education = () => {
       animate={{ opacity: 1, transition: { duration: 0.5 } }}
       className="w-full"
     >
-      {/* part one */}
       <div>
-        {/* <div className="py-6 lgl:py-12 font-titleFont flex flex-col gap-4">
-          <h2 className="text-3xl md:text-4xl font-bold">
-            Education Quality and Job Experience
-          </h2>
-        </div> */}
         <div className="mt-6 lgl:mt-14 w-full h-fit m-auto border-l-[6px] border-l-black border-opacity-30 flex items-center justify-center flex-col gap-10">
           <h2 className="text-3xl md:text-4xl font-bold">Education Quality</h2>
           <ResumeCard
@@ -24,18 +18,19 @@ const Education = () => {
             result="Syria"
           />
           <h2 className="text-3xl md:text-4xl font-bold">Job Experience</h2>
-          {/* <ResumeCard
-            title="IT Support Engineer"
-            subTitle="Andron Business Group - (2021/11/11 - Present)"
-            result="Syria"
-            des="I provide technical assistance to end-users, resolving hardware and software issues, configuring systems, and offering network support. I ensure smooth IT operations and help users navigate technology-related challenges."
-          /> */}
           <ResumeCard
             title="Frontend Web developer"
-            subTitle="Divcodes - (2022 - Present)"
+            subTitle="Divcodes - (2021 - 31/01/2024)"
             result="Lebanon"
-            // subTitle="Freelancer (2022 - Present)"
-            des="I specialize in creating user-friendly interfaces and exceptional user experiences. I have experience working with modern build tools like Vite and Nx monorepo. I am proficient in integrating applications with backend systems using Restful API and GraphQL. I have also worked with real-time technologies such as Socket.io, Firebase, and Google Maps services. Additionally, I have expertise in payment gateway integration. My focus is on delivering intuitive interfaces and seamless user experiences."
+            des="creating user-friendly interfaces and exceptional user experiences. I have experience working with modern build tools like Vite and Nx monorepo. I am proficient in integrating applications with backend systems using Restful API and GraphQL. I have also worked with real-time technologies such as Socket.io, Firebase, and Google Maps services. Additionally, I have expertise in payment gateway integration. My focus is on delivering intuitive interfaces and seamless user experiences."
+          />
+
+          <ResumeCard
+            title="Frontend Web developer"
+            subTitle="FREELANCER - ( 2020 - 2021 )"
+            result="Syria"
+            des="Designed responsive landing pages using HTML5 and CSS. Effective collaboration with clients and team members
+            was a key element of my success"
           />
         </div>
       </div>
